96dfbaadcac6e92bacf53988a24efcab6e0c5fd4
Ravi Kumar <84226388+thatOMSguy@users.noreply.github.com>
2023-04-17 18:50:41 +0530

    flux operations

--- src/test/java/com/myspringlearning/spring6reactive/repositories/PersonRepositoryImplTest.java
+++ src/test/java/com/myspringlearning/spring6reactive/repositories/PersonRepositoryImplTest.java
@@ -2,7 +2,10 @@ package com.myspringlearning.spring6reactive.repositories;
 
 import com.myspringlearning.spring6reactive.domain.Person;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,9 +39,56 @@ class PersonRepositoryImplTest {
     void testMapOperation() {
         Mono<Person> personMono = personRepository.getById(1);
 
-        personMono.map(person -> person.getFirstName()).subscribe(firstName -> System.out.println(firstName));
-
-
+        personMono.map(Person::getFirstName).subscribe(firstName -> System.out.println(firstName));
     }
+
+    @Test
+    void fluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+        Person person = personFlux.blockFirst();
+        System.out.println(person);
+        Person person2 = personFlux.blockLast();
+        System.out.println(person2);
+    }
+
+    @Test
+    void testFluxSubscriber() {
+
+        Flux<Person> personFlux = personRepository.findAll();
+
+
+        personFlux.subscribe(
+                person -> {
+                    System.out.println(person);
+                }
+        );
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.map(Person::getFirstName).subscribe(firstName ->
+                System.out.println(firstName));
+    }
+
+    @Test
+    void testFluxToMonoList() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+
+        Mono<List<Person>> personMonoList = personFlux.collectList();
+
+        personMonoList.subscribe(
+                list -> {
+                    list.forEach(
+                            person -> {
+                                System.out.println(person.getFirstName());
+                            }
+                    );
+                }
+        );
+    }
+
 
 }
